13934d68124f5004786a3cd63f871b76600d99bd
Westar35 <170506694+Westar35@users.noreply.github.com>
2026-03-28 00:30:53 +0300

    Base HTTP

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"net/http"
+	"log"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "server is running")
+	})
+	log.Println("starting server on :8080")
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
