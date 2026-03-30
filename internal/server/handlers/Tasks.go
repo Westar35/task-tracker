5834d42865d65925ce2c699d04cd0c46d491afed
Westar35 <170506694+Westar35@users.noreply.github.com>
2026-03-30 16:34:13 +0300

    Handlers Update

--- internal/server/handlers/Tasks.go
+++ internal/server/handlers/Tasks.go
@@ -1,14 +1,30 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
+	"fmt"
 )
+
+type Task struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+}
 
 func TasksHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		GetTasksHandler(w, r)
+		tasks := []Task{
+			{ID: 1, Title: "Task 1"},
+		}
+		json.NewEncoder(w).Encode(tasks)
+		//GetTasksHandler(w, r)
 	case http.MethodPost:
-		CreateTaskHandler(w, r)
+		var task Task
+		json.NewDecoder(r.Body).Decode(&task)
+		fmt.Fprintf(w, "Создана задача: %s", task.Title)
+		//CreateTaskHandler(w, r)
+	default:
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 	}
 }
