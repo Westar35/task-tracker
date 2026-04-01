75da8893883af8ab02d2ed98be8a0a2fd97c688b
Westar35 <170506694+Westar35@users.noreply.github.com>
2026-04-01 16:19:13 +0300

    1234

--- internal/handlers/tasks.go
+++ internal/handlers/tasks.go
@@ -3,9 +3,6 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
-	"strconv"
-	"strings"
-	"task-tracker/internal/models"
 	"task-tracker/internal/storage"
 )
 
@@ -19,90 +16,21 @@ func NewTaskHandler(taskStorage *storage.TaskStorage) *TaskHandler {
 	}
 }
 
-// Нужно перенести инициализацию в run() и передавать в tasks.go
-var taskStorage = storage.NewTaskStorage()
-
-func TasksHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		getTasksHandler(w, r)
-	case http.MethodPost:
-		createTaskHandler(w, r)
-	default:
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-	}
-}
-
 func (h *TaskHandler) Tasks(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		getTasksHandler(w, r)
-	case http.MethodPost:
-		createTaskHandler(w, r)
+		h.getAllTasks(w, r)
 	default:
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
 
-func createTaskHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	var req models.CreateTaskRequest
-	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil && req.Title != "" {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-	newTask := taskStorage.CreateTask(req.Title)
-	json.NewEncoder(w).Encode(newTask)
-	taskStorage.AddTask(newTask)
-}
-
-func getTasksHandler(w http.ResponseWriter, r *http.Request) {
+func (h *TaskHandler) getAllTasks(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	json.NewEncoder(w).Encode(taskStorage.GetAllTasks())
+	json.NewEncoder(w).Encode(h)
 }
 
-func TaskByIDHandler(w http.ResponseWriter, r *http.Request) {
-	parts := strings.Split(r.URL.Path, "/")
-	if len(parts) != 3 {
-		http.Error(w, "Invalid path", http.StatusBadRequest)
-		return
-	}
-	id, err := strconv.Atoi(parts[2])
-	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
-		return
-	}
-	switch r.Method {
-	case http.MethodGet:
-		getTaskByIDHandler(w, r, id)
-	default:
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-	}
-}
-
-func getTaskByIDHandler(w http.ResponseWriter, r *http.Request, id int) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	task, err := taskStorage.GetTaskByID(id)
-	if err != nil {
-		http.Error(w, "Task not found", http.StatusNotFound)
-		return
-	}
-	json.NewEncoder(w).Encode(task)
-}
-
-func DeleteTaskByIDHandler(w http.ResponseWriter, r *http.Request, id int) {
-	if r.Method != http.MethodDelete {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-}
+//123456
